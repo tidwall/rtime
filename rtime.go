639749c3324c35357b8d85a9ef4882f9bb2c9307
Steve Manuel <smanuel@recurly.com>
2020-04-12 22:53:51 -0600

    alt: use channels to sync and timeout

--- rtime.go
+++ rtime.go
@@ -26,10 +26,8 @@ var rtime time.Time
 //    }
 //
 func Now() time.Time {
-	var mu sync.Mutex
 	res := make([]time.Time, 0, len(sites))
-	cond := sync.NewCond(&mu)
-	var timedout bool
+	results := make(chan time.Time)
 
 	// get as many dates as quickly as possible
 	client := http.Client{
@@ -45,69 +43,58 @@ func Now() time.Time {
 				tm, err := time.Parse(time.RFC1123, resp.Header.Get("Date"))
 				resp.Body.Close()
 				if err == nil {
-					mu.Lock()
-					res = append(res, tm)
-					cond.Broadcast()
-					mu.Unlock()
+					results <- tm
 				}
 			}
 		}(site)
 	}
 
-	go func() {
-		// timeout after two second
-		time.Sleep(time.Second * 2)
-		mu.Lock()
-		timedout = true
-		cond.Broadcast()
-		mu.Unlock()
-	}()
-
-	mu.Lock()
-	defer mu.Unlock()
 	for {
-		if len(res) >= 3 {
-			// We must have a minimum of three results. Find the two of three
-			// that have the least difference in time and take the smaller of
-			// the two.
-			type pair struct {
-				tm0  time.Time
-				tm1  time.Time
-				diff time.Duration
-			}
-			var list []pair
-			for i := 0; i < len(res); i++ {
-				for j := i + 1; j < len(res); j++ {
-					if i != j {
-						tm0, tm1 := res[i], res[j]
-						if tm0.After(tm1) {
-							tm0, tm1 = tm1, tm0
+		select {
+		case <-time.After(2 * time.Second):
+			return time.Time{}
+		case tm := <-results:
+			res = append(res, tm)
+
+			if len(res) >= 3 {
+				// We must have a minimum of three results. Find the two of three
+				// that have the least difference in time and take the smaller of
+				// the two.
+				type pair struct {
+					tm0  time.Time
+					tm1  time.Time
+					diff time.Duration
+				}
+				var list []pair
+				for i := 0; i < len(res); i++ {
+					for j := i + 1; j < len(res); j++ {
+						if i != j {
+							tm0, tm1 := res[i], res[j]
+							if tm0.After(tm1) {
+								tm0, tm1 = tm1, tm0
+							}
+							list = append(list, pair{tm0, tm1, tm1.Sub(tm0)})
 						}
-						list = append(list, pair{tm0, tm1, tm1.Sub(tm0)})
 					}
 				}
-			}
-			sort.Slice(list, func(i, j int) bool {
-				if list[i].diff < list[j].diff {
-					return true
+				sort.Slice(list, func(i, j int) bool {
+					if list[i].diff < list[j].diff {
+						return true
+					}
+					if list[i].diff > list[j].diff {
+						return false
+					}
+					return list[i].tm0.Before(list[j].tm0)
+				})
+				res := list[0].tm0.Local()
+				// Ensure that the new time is after the previous time.
+				rmu.Lock()
+				defer rmu.Unlock()
+				if res.After(rtime) {
+					rtime = res
 				}
-				if list[i].diff > list[j].diff {
-					return false
-				}
-				return list[i].tm0.Before(list[j].tm0)
-			})
-			res := list[0].tm0.Local()
-			// Ensure that the new time is after the previous time.
-			rmu.Lock()
-			defer rmu.Unlock()
-			if res.After(rtime) {
-				rtime = res
+				return rtime
 			}
-			return rtime
 		}
-		if timedout {
-			return time.Time{}
-		}
-		cond.Wait()
 	}
 }
